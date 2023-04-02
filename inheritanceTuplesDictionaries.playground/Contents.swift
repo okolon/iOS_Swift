82d5a4c29f3156eaa3afa1d66a0a1ce0796ab86f
okolon <normanokolo@gmail.com>
2023-04-02 20:32:59 +0100

    finished class

--- inheritanceTuplesDictionaries.playground/Contents.swift
+++ inheritanceTuplesDictionaries.playground/Contents.swift
@@ -1,51 +1,51 @@
 import UIKit
 
 // dictionary example
-let avgPrice = [
-    "Golden Dragon": 5.98,
-    "Half Moon Bay": 5.17
+let avgRate = [
+    "Golden Dragon": 4.59,
+    "Half Moon Bay": 4.17
 ]
 
-for (key, value) in avgPrice {
-    if value < 5.50 {
+for (key, value) in avgRate {
+    if value < 4.50 {
         print("The cheapest is \(key)")
     }
 }
 
 //tuples example
-var chineseShops = (name: "Golden Dragon", rating: 4.1)
+var chineseShops = (name: "Golden Dragon", rate: 4.1)
 
 func getRating() -> Double {
-    return chineseShops.rating
+    return chineseShops.rate
 }
 
 print("I want that shop with a rating of \(getRating())")
 
 //inheritance exmaple
 
-class  Cuisine {
-    var type: String
-    var menuSize: String
+class Cuisine {
+    let type: String
     
-    init(type: String, menuSize: String) {
+    init(type: String) {
         self.type = type
-        self.menuSize = menuSize
     }
 }
 
 class Chinese: Cuisine {
     var name: String
-    var avgPrice: Double
     var rating: Double
+    var avgPrice: Double
     
-    init(name: String, avgPrice: Double, rating: Double) {
+    init(name: String, rating: Double, avgPrice: Double) {
         self.name = name
-        self.avgPrice = avgPrice
         self.rating = rating
+        self.avgPrice = avgPrice
+        
+        super.init(type: "Chinese")
     }
 }
 
+let goldenD = Chinese(name: "Golden Dragon", rating: 4.59, avgPrice: 5.90)
 
-var newChinese = Chinese(name: "Golden Dragon", avgPrice: 5.98, rating: 4.1)
+print(goldenD.avgPrice)
 
-print(newChinese.avgPrice)
